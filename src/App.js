9623218c3870b2d6e43a0c9fac1ad491ca2823a7
Zprogrammer-code <skatboyza124@hotmail.com>
2020-05-13 14:27:03 -0600

    testing rebuild of mod 4 Capstone after burning it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header'
+import MemeGenerator from './MemeGenerator'
+// import PhotoLibrary from './containers/PhotoLibrary'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+      constructor(){
+        super()
+        this.state = {
+          topText: "",
+          bottomText: "",
+          randomImg: "http://i.img"
+        }
+      }
+
+  render(){
+      return (
+        <div className="App">
+          <Header />
+          <MemeGenerator />
+        </div>
+      );
+    }
 }
+
 
 export default App;
